63977ed7c88b4f3af13406f228a6b212b70f4963
shiv pujari <shivanipujari03@gmail.com>
2023-03-01 09:21:26 +0530

    reducrehook

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,41 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import '../styles/App.css';
+
 const App = () => {
+
+
+ const initialTaskState={
+  count:0,
+ }
+
+
+
+  const reducer =(state, action)=>{
+    const{type, payload}= action;
+    switch(action.type){
+      case "add":
+        return{
+          ...state,
+          count :state.count + 1,
+        }
+    case "delete":
+      return{
+        ...state,
+        count:state.count-1,
+      }
+      case "deleteAll":
+        return{
+        ...state,
+        count: 0,
+
+        }
+        default:
+          return state;
+      }
+  }
+
+
+  const [taskState, dispatch] = useReducer(reducer, initialTaskState)
 
   return (
     <div id="main">
